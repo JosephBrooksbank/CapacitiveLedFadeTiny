1bbf8ecbf5a2bdeaa3bbf9657db4b9ce1cf03eeb
Joseph Brooksbank <Joseph.Brooksbank@du.edu>
2024-10-05 14:07:57 -0500

    Send / Receive i2c main board

--- Serial_i2c_Controller/src/main.cpp
+++ Serial_i2c_Controller/src/main.cpp
@@ -1,12 +1,22 @@
 #include <Arduino.h>
+#include <Wire.h>
+
 void setup() {
     Serial.begin(9600);
-    Serial1.begin(9600);
+    Wire.begin();
 }
 
+byte x = 0;
 void loop() {
-    if (Serial1.available()) {
-        char c = char(Serial1.read());
+    Wire.beginTransmission(4);
+    Wire.write('x' );
+    Wire.write(x);
+    Wire.endTransmission();
+    Wire.requestFrom(4, 5);
+    while (0 < Wire.available()) {
+        char c = Wire.read();
         Serial.print(c);
     }
+    Serial.println();
+    delay(500);
 }
