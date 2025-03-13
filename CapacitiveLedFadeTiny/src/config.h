4bb1639033ccb483bcf4cff46c631026ebed45ee
Joseph Brooksbank <joe.brooksbank@gmail.com>
2025-03-13 09:06:31 -0500

    Wire working
    also optional software serial as a backup

--- CapacitiveLedFadeTiny/src/config.h
+++ CapacitiveLedFadeTiny/src/config.h
@@ -7,13 +7,27 @@
 #define OFF_DELAY 200
 
 #define BAUD_RATE 115200
-#define I2C_ADDRESS 14
 #define NUM_LEDS 30
-#define ENABLE_DEBUG_LED_PIN PIN_A7
-#define STATUS_PIN PIN_A4
-#define SENSE_PIN PIN_A6
+#define ENABLE_DEBUG_LED_PIN PIN_PA7
+#define STATUS_PIN PIN_PA4
+#define SENSE_PIN PIN_PA6
 #define LED_DRIVER WS2812
-#define LED_PIN PIN_A5
+#define LED_PIN PIN_PA5
+
+
+// i2c stuff
+#define USE_I2C
+#define I2C_ADDRESS 14
+
+
+// Software Serial Stuff
+#ifndef USE_I2C
+    #define USE_SOFTWARE_SERIAL
+    #define SOFTWARE_BAUD 9600
+    #define RX_PIN PIN_PB0
+    #define TX_PIN PIN_PB1
+#endif
+
 
 #define USE_ONBOARD_LED
 #define USE_STATUS_LED
--- CapacitiveLedFadeTiny/src/main.cpp
+++ CapacitiveLedFadeTiny/src/main.cpp
@@ -4,20 +4,23 @@
 #include "status_led.h"
 #include "config.h"
 #include "millis_fix.h"
+#include "SoftwareSerial.h"
 
 
 
 void receiveDataWire(int numBytes);
-volatile byte i2c_buffer[30];
-volatile uint16_t i2c_length = 0;
+volatile byte input_buffer[30];
+volatile uint8_t input_length = 0;
+volatile bool new_message = false;
 int referenceCap = 0;
 bool lightOn = false;
 CRGB leds[NUM_LEDS];
-uint16_t cycles_since_last_touched = 0;
+uint16_t cycles_since_last_touched = OFF_DELAY;
 
 void setup_i2c() {
-    Wire.begin(I2C_ADDRESS);
+    Wire.begin(I2C_ADDRESS, true);
     Wire.onReceive(receiveDataWire);
+    Serial.println("Using i2c");
 
 }
 
@@ -25,15 +28,28 @@ void setup_leds() {
     FastLED.addLeds<LED_DRIVER, LED_PIN, GRB>(leds, NUM_LEDS);
 }
 
+#ifdef USE_SOFTWARE_SERIAL
+    SoftwareSerial soft_serial = SoftwareSerial(RX_PIN, TX_PIN);
+#endif
+
 void setup() {
     Serial.begin(BAUD_RATE);
-    setup_i2c();
+    #ifdef USE_I2C
+        setup_i2c();
+    #endif
+    #ifdef USE_SOFTWARE_SERIAL
+        pinMode(RX_PIN, INPUT);
+        pinMode(TX_PIN, OUTPUT);
+        soft_serial.begin(SOFTWARE_BAUD);
+        Serial.println("Using software serial");
+
+    #endif
     setup_leds();
     setup_status_led();
     FastLED.setBrightness(50);
     #ifdef USE_ONBOARD_LED
         pinMode(ENABLE_DEBUG_LED_PIN, OUTPUT); 
-        digitalWrite(ENABLE_DEBUG_LED_PIN, true);
+        digitalWriteFast(ENABLE_DEBUG_LED_PIN, true);
     #endif
 
     referenceCap = read(SENSE_PIN, 64);
@@ -43,7 +59,28 @@ void setup() {
 
 int reading = 0;
 void loop() {
-    Serial.println("looping");
+
+    #ifdef USE_SOFTWARE_SERIAL
+    if (soft_serial.available()) {
+        new_message = true;
+        input_length = 0;
+        while (soft_serial.available()) {
+            input_buffer[input_length++] = soft_serial.read();
+        }
+    }
+    #endif
+
+    if (new_message) {
+        new_message = false;
+        Serial.printf("Received message of length %d\n", input_length);
+        Serial.print("Message: ");
+        for (uint8_t i = 0; i < input_length; i++) {
+            Serial.print((char)input_buffer[i]);
+        }
+        Serial.println();
+    }
+
+
     reading = read(SENSE_PIN, 64) - referenceCap;
 
     if (reading >= TOUCH_SENSE) {
@@ -69,7 +106,8 @@ void loop() {
 
 void receiveDataWire(int numBytes) {
     for (uint8_t i = 0; i < numBytes; i++) {
-        i2c_buffer[i] = Wire.read();
+        input_buffer[i] = Wire.read();
     }
-    i2c_length = numBytes;
+    input_length = numBytes;
+    new_message = true;
 }
