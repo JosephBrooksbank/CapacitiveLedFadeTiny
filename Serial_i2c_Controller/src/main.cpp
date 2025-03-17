0e25b86ad3ee35d7a43fffde04c793ec0b23a01b
Joseph Brooksbank <joseph.brooksbank@navex.com>
2025-03-16 23:35:39 -0500

    set scl manually
    the default wire frequency setting method does not make use of the prescaler, so extremely slow speeds are not available

--- Serial_i2c_Controller/src/main.cpp
+++ Serial_i2c_Controller/src/main.cpp
@@ -6,14 +6,29 @@ Config testConfig = {
     .version = 1,
     .touch_sense = DEFAULT_TOUCH_SENSE,
     .off_delay = DEFAULT_OFF_DELAY,
-    .max_brightness = 50,
+    .max_brightness = 100,
     .color_mode = ColorMode::Random,
     .single_color_color = CRGB::Blue
 };
 
+void set_scl_min() {
+    // the absolute slowest that SCL can run according to the hardware,
+    // about 500hz
+    TWSR |= 1 << TWPS0;
+    TWSR |= 1 << TWPS1;
+    TWBR = 0b11111111;
+}
+
+void set_scl_max() {
+    TWSR &= ~(1 << TWPS0);
+    TWSR &= ~(1 << TWPS1);
+    TWBR = 12;
+
+}
+
 void setup() {
     Wire.begin();
-    Wire.setClock(500);
+    set_scl_max();
 
     Wire.beginTransmission(0);
     Wire.write((byte*)&testConfig, sizeof(Config));
